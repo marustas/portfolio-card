421fd60f1072b844de314c6d2d6b3305a3ae9b10
Stanislau Marudau <marudov.stas@gmail.com>
2023-07-20 19:23:13 +0300

    Vital components added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
     </div>
   );
 }
 
+const Avatar = () => {
+  return <img src="profile.jpg" alt="profile" className="avatar" />;
+};
+
+const Skill = () => {
+  return <div></div>;
+};
+
+const SkillList = () => {
+  return <Skill />;
+};
+
+const Intro = () => {
+  return (
+    <div>
+      <h1>Stanisalu Marudau</h1>
+      <p>
+        Hi, I'm a determined and goal-orineted Software engineer in learning,
+        studying at VGTU, participant of NWERC22 semi-finals and organizer of
+        startup AI2tor. I enjoy playing tennis and basketball.
+      </p>
+    </div>
+  );
+};
 export default App;
